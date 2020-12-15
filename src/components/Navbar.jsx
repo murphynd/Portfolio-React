dff5173a910abefabd94577f9818ff357804c1bf
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-12-15 14:02:19 -0800

    adds css to app, google font, Nav bar complete,

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
 import "./Navbar.css";
@@ -18,11 +18,16 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    showButton();
+  }, []);
+
   window.addEventListener("resize", showButton);
+
   return (
     <div className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
           Portfolio <i className="fas fa-air-freshener" />
         </Link>
         <div className="menu-icon" onClick={handleClick}>
