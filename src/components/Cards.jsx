dc5dd90a599126318121614ad16e5eb9f36a881c
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-12-15 15:16:55 -0800

    picture links

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,7 +9,20 @@ function Cards() {
       <div className="cards__contianer">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem src="images/den.png" text="" label="" path="/about" />
+            {" "}
+            {/* you will need to duplicate the ul tag closure for good wrapping */}
+            <CardItem
+              src="images/den.png"
+              text="have a seat"
+              label="a cozy room"
+              path="/about"
+            />
+            <CardItem
+              src="images/tea.png"
+              text="here is some tea"
+              label="a hot cup"
+              path="/about"
+            />
           </ul>
         </div>
       </div>
