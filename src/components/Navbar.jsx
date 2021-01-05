10f8a6f939022611eaa7ab82c8539e485e1abcaf
Natalie Murphy <Nataliemurphy500@gmail.com>
2021-01-05 12:22:33 -0800

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "./Button";
+// import { Button } from "./Button";
 import "./Navbar.css";
 
 function Navbar() {
@@ -59,7 +59,7 @@ function Navbar() {
             </Link>
           </li>
         </ul>
-        {button && <Button buttonStyle="btn--outline">CONTACT</Button>}
+        {/* {button && <Button buttonStyle="btn--outline">CONTACT</Button>} */}
       </div>
     </div>
   );
